832bfefc146ca91e83bb8fa6e80f684083c2eed7
qiulihua83 <qiulihua83@163.com>
2015-02-09 16:12:18 +0800

    add helloworld model test

--- app/code/local/App/Helloworld/controllers/BlogController.php
+++ app/code/local/App/Helloworld/controllers/BlogController.php
@@ -6,7 +6,10 @@ class App_Helloworld_BlogController extends Mage_Core_Controller_Front_Action{
     
     public  function indexAction()
     {
-        echo "welcome to blog.";
+        Mage::log("in Blogpost class index();");
+        //echo "welcom to blog.";
+         $blogpost = Mage::getModel('helloworld/blogpost');
+         echo get_class($blogpost);
     }
     
     
---
+++ app/code/local/App/Helloworld/model/Blogpost.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class App_Helloworld_Model_Blogpost extends Mage_Core_Model_Abstract{
+    
+    
+    
+    protected function _construct()
+    {
+        $this->_init('helloworld/blogpost');
+    }
+    
+    
+    
+}
+
