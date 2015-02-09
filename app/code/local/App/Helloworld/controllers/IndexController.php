e04670a8bf99bf244bc7093078a2506b75e60604
qiulihua83 <qiulihua83@163.com>
2015-02-09 12:22:29 +0800

    add method

--- app/code/local/App/Helloworld/controllers/IndexController.php
+++ app/code/local/App/Helloworld/controllers/IndexController.php
@@ -9,5 +9,9 @@ class App_Helloworld_IndexController extends Mage_Core_Controller_Front_Action{
         echo "welcom to magentotest.";
     }
     
+    public  function goodbyeAction()
+    {
+        echo 'welcome next time.';
+    }
     
 }
