b4d3436e7b31849dba6401d32e7cf30c57109839
qiulihua83 <qiulihua83@163.com>
2015-02-10 17:17:35 +0800

    install test

--- app/code/local/App/Helloworld/sql/helloworld_setup/mysql4-install-0.1.0.php
+++ app/code/local/App/Helloworld/sql/helloworld_setup/mysql4-install-0.1.0.php
@@ -1,5 +1,31 @@
 <?php
+echo "Running this upgrade: " . get_class ( $this ) . "<br/>";
 
-echo "Running this upgrade: ".get_class($this)."<br/>";
+$installer = $this;
+$installer->startSetup ();
+$sql = "CREATE TABLE {$installer->getTable('helloworld/blogpost')} (
+blogpost_id int(11) NOT NULL auto_increment,
+title text,
+post text,
+date datetime default NULL,
+timestamp timestamp NOT NULL default CURRENT_TIMESTAMP,
+PRIMARY KEY (blogpost_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO {$installer->getTable('helloworld/blogpost')} VALUES (1,'My New Title','This is a blog post','2009-07-01 00:00:00','2009-07-02 23:12:30');
+";
+echo "".$sql."<br/>";
+$installer->Run($sql);
 
-//die( "die now.......");
+// $table = $installer->getConnection()
+//               ->newTable($installer->getTable('helloworld/blogpost'))
+//               ->addColumn("blogpost_id",Varien_Db_Ddl_Table::TYPE_INTEGER,null, array(
+//                                         'identity'  => true,
+//                                         'unsigned'  => true,
+//                                         'nullable'  => false,
+//                                         'auto_increment' => true,
+//                                         'primary'   => true), 'ID');
+
+
+$installer->endSetup();
+
+echo "install end.";
